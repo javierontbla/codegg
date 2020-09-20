ad63bd61b280410bf8ad61804a34f09018ade708
Javier Ontiveros <javierontiveros@Javiers-MacBook-Air.local>
2020-09-20 00:31:29 -0500

    deleted redux-logger from project

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import logger from "redux-logger";
 
 import root from "./root";
 import { rootSaga } from "./root.saga";
 
 // saga initialization
 const saga = createSagaMiddleware();
-const middlewares = [saga, logger];
+const middlewares = [saga];
 
 const store = createStore(root, applyMiddleware(...middlewares));
 // all saga actions in one function
