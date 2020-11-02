1ef29be45e00e6fb3a89d3c8631f234ddcdc26f0
Javier Ontiveros <javierontiveros@Javiers-MacBook-Air.local>
2020-11-02 15:45:38 -0600

    changed width on article container to 58%

--- src/pages/article.page/components/Article.styles.js
+++ src/pages/article.page/components/Article.styles.js
@@ -107,7 +107,7 @@ export const Body = styled.div`
   padding: 0.9rem;
   margin-top: 3rem;
   margin-bottom: 2vh;
-  width: 50%;
+  width: 58%;
   text-align: left;
 
   @media (max-width: 500px) {
